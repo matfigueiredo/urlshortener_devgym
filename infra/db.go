1e9551db7912d0e772705be41698b04b7709c437
Matheus Figueiredo <figo.ppv@gmail.com>
2023-11-02 12:09:05 -0300

    fix: (db) fix connection

--- infra/db.go
+++ infra/db.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	_ "github.com/lib/pq"
+	"github.com/matfigueiredo/urlshortener_devgym/domain"
 )
 
 func Connect() (*sql.DB, error) {
@@ -30,4 +31,24 @@ func TestConnection() {
 
 	fmt.Println("Conexão estabelecida com sucesso!")
 
+}
+
+type URLRepositoryDB struct {
+	db *sql.DB
+}
+
+func NewURLRepositoryDB() *URLRepositoryDB {
+	db, _ := Connect() // Melhor manejar este erro em produção
+	return &URLRepositoryDB{db: db}
+}
+
+func (r *URLRepositoryDB) Save(url domain.URL) error {
+	_, err := r.db.Exec("INSERT INTO urls(original, code) VALUES($1, $2)", url.Original, url.Code)
+	return err
+}
+
+func (r *URLRepositoryDB) FindByCode(code string) (domain.URL, error) {
+	var url domain.URL
+	err := r.db.QueryRow("SELECT original, code FROM urls WHERE code = $1", code).Scan(&url.Original, &url.Code)
+	return url, err
 }
