405968355b58048872327f864c528d660d6efc32
Matheus Figueiredo <figo.ppv@gmail.com>
2023-11-02 20:28:38 -0300

    fix

--- interface/http/handlers.go
+++ interface/http/handlers.go
@@ -10,9 +10,9 @@ type Handlers struct {
 }
 
 func (h *Handlers) ShortenURL(c *gin.Context) {
-	// Handler para encurtar a URL
+
 }
 
 func (h *Handlers) RedirectURL(c *gin.Context) {
-	// Handler para redirecionar pela URL encurtada
+
 }
